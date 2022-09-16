606e268238aaf977bb9e341652666af20ef95af4
lsouzatj <lsouza.tj@gmail.com>
2022-09-16 20:24:47 -0300

    Initial commit

---
+++ eurekaserver/src/main/java/com/br/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,14 @@
+package com.br.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
