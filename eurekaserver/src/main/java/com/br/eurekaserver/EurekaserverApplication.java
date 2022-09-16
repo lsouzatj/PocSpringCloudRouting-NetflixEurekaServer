c3fa825b3a9c1ab003bb6305f482a5537b668cfc
lsouzatj <lsouza.tj@gmail.com>
2022-09-16 20:44:57 -0300

    Criacao do projeto

--- eurekaserver/src/main/java/com/br/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/br/eurekaserver/EurekaserverApplication.java
@@ -2,7 +2,6 @@ package com.br.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 public class EurekaserverApplication {
